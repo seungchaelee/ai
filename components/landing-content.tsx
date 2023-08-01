df73c1828a42bd9d95acc844c0ff1695c6a24ccb
seungchae <id6639@naver.com>
2023-08-01 21:03:54 +0900

    Wrap Up

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -4,27 +4,27 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Joel",
-    avatar: "J",
+    name: "Lee",
+    avatar: "L",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
   },
   {
-    name: "Antonio",
-    avatar: "A",
+    name: "Lee",
+    avatar: "L",
     title: "Designer",
     description: "I use this daily for generating new photos!",
   },
   {
-    name: "Mark",
-    avatar: "M",
+    name: "Lee",
+    avatar: "L",
     title: "CEO",
     description: "This app has changed my life, cannot imagine working without it!",
   },
   {
-    name: "Mary",
-    avatar: "M",
-    title: "CFO",
+    name: "Lee",
+    avatar: "L",
+    title: "CTO",
     description: "The best in class, definitely worth the premium subscription!",
   },
 ];
